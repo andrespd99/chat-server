fcaa1452c993c0fe0ff1d1f2d0bfaac604734e8e
andrespd99 <andres.epacheco99@gmail.com>
2022-07-06 14:32:27 -0400

    Clase 116

---
+++ models/message.js
@@ -0,0 +1,35 @@
+const {
+    Schema,
+    model
+} = require('mongoose');
+
+const MessageSchema = new Schema({
+    from: {
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    message: {
+        type: String,
+        required: true,
+    },
+    to: {
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    }
+}, {
+    timestamps: true
+});
+
+MessageSchema.method('toJSON', function () {
+    const {
+        __v,
+        _id,
+        ...object
+    } = this.toObject();
+
+    return object;
+});
+
+module.exports = model('Message', MessageSchema);
